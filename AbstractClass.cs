1401b99ce1006181acdaf0fd167303ee1299fb29
venky <venky94.vk@gmail.com>
2023-05-01 11:57:54 +0530

    [Venkatesh] Files added

---
+++ AbstractClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+   public class AbstractClass 
+    {
+       /* public static void Main(string[] args)
+        {
+            ABC abc = new XYZ2();
+            abc.connection();
+            abc.saveData();
+
+        }*/
+
+       
+    }
+    public abstract class ABC : A
+    {
+
+        public virtual void connection() { }
+        public void saveData()
+        {
+            Console.WriteLine("Data Saved");
+        }       
+
+    }
+    public class XYZ : ABC
+    {
+        public override void connection()
+        {
+            Console.WriteLine("Connection Established wit sql");
+        }
+    }
+    public class XYZ2 : ABC
+    {
+        public override void connection()
+        {
+            Console.WriteLine("connection established with MondoDb");
+        }
+    }
+}
---
+++ Encapulation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    internal class Student
+    {
+        public Student(int id)
+        {
+            Id = id;
+        }
+        private int age;
+        public string Name;
+        public int Id { get; }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    age = value;
+                }
+            }
+            
+        }
+        public static void Main(string[] args)
+        {
+            Student ashok=new Student(54);
+            ashok.Name = "Ashok";
+            ashok.Age = 80;
+
+            Student rakesh=new Student(34);
+            rakesh.Name = "Rakesh";
+            rakesh.Age = 35;
+
+
+            Console.WriteLine(ashok.Id);
+            Console.WriteLine(ashok.Age);
+
+        }
+     
+    }
+}
